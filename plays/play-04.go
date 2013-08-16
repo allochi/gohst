faa3a7986b80b72ed3ede9d2ba866d58f61390fb
Ali Anwar <allochi@gmail.com>
2013-08-16 17:57:35 +0200

    Working Sample with proper store deligate, problem with response

--- plays/play-04.go
+++ plays/play-04.go
@@ -23,15 +23,21 @@ func main() {
 	// people = append(people, vanessa)
 
 	DataStore = People
-	fmt.Printf("Main (people b4PUT): %s\n", people)
-	PUT(allochi)
-	PUT(vanessa)
-	fmt.Printf("Main (people A8PUT): %s\n", people)
+	fmt.Printf("Main (people b4PUT): %s\n", People)
+	response := PUT(allochi)
+	fmt.Println(response)
+	if PUT(allochi).Type == "OK" {
+		fmt.Printf("$v inserted", allochi)
+	}
+	if PUT(vanessa).Type == "OK" {
+		fmt.Printf("$v inserted", vanessa)
+	}
+	fmt.Printf("Main (people A8PUT): %s\n", People)
 
-	// DataStore = Others
-	// fmt.Printf("Main (others b4PUT): %s\n", others)
-	// PUT(allochi)
-	// fmt.Printf("Main (others A8PUT): %s\n", others)
+	DataStore = others
+	fmt.Printf("Main (others b4PUT): %s\n", others)
+	PUT(allochi)
+	fmt.Printf("Main (others A8PUT): %s\n", others)
 
 }
 
@@ -46,6 +52,7 @@ type DataStoreDeligate interface {
 var DataStore DataStoreDeligate
 
 type Response struct {
+	Type  string
 	Error error
 	Size  int
 }
@@ -66,28 +73,40 @@ func GET(object *interface{}, query interface{}) Response {
 }
 
 // --------------------------------------------------------------------------------
-// Data Stores
+// Array Data Stores
 // --------------------------------------------------------------------------------
-var people []Person
+type PeopleDataStore []Person
+
 var People PeopleDataStore
 
-type PeopleDataStore DataStoreDeligate
-
-func Put(object interface{}) {
+func (ds PeopleDataStore) PUT(object interface{}) Response {
 	_object := reflect.ValueOf(object)
-	_people := reflect.Indirect(reflect.ValueOf(&people))
+	_people := reflect.Indirect(reflect.ValueOf(&People))
 	_people.Set(reflect.Append(_people, _object))
+
+	response := Response{}
+	response.Type = "asjdhalhdlas"
+	response.Error = nil
+	response.Size = 100
+	fmt.Println(response)
+	return response
 }
 
-func GET(*interface{}, interface{}) Response {
+func (ds PeopleDataStore) GET(*interface{}, interface{}) Response {
 	return Response{}
 }
 
-// var others []Person
-// var Others DataStoreDeligate
+type OthersDataStore []Person
 
-// func OthersPut(object interface{}) {
-// 	_object := reflect.ValueOf(object)
-// 	_others := reflect.Indirect(reflect.ValueOf(&others))
-// 	_others.Set(reflect.Append(_others, _object))
-// }
+var others OthersDataStore
+
+func (ds OthersDataStore) PUT(object interface{}) Response {
+	_object := reflect.ValueOf(object)
+	_others := reflect.Indirect(reflect.ValueOf(&others))
+	_others.Set(reflect.Append(_others, _object))
+	return Response{}
+}
+
+func (ds OthersDataStore) GET(*interface{}, interface{}) Response {
+	return Response{}
+}
