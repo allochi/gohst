4e698c9921179910bcee8b384955f622ace56df1
Ali Anwar <allochi@gmail.com>
2013-10-10 16:57:09 +0200

    Playing with goroutine and gohst

---
+++ plays/play-12.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"allochi/gohst"
+	. "allochi/gohst/plays/models"
+	. "allochi/tcolor"
+	"fmt"
+	"log"
+	"time"
+)
+
+var tc = TColor
+
+var Contactizer gohst.DataStore
+
+func init() {
+
+	ContactizerJson := gohst.NewPostJson("allochi_contactizer", "allochi", "")
+	ContactizerJson.CheckCollections = true
+	ContactizerJson.AutoCreateCollections = true
+
+	Contactizer.Register("Contactizer", ContactizerJson)
+	Contactizer.Connect()
+}
+
+func main() {
+
+	defer Contactizer.Disconnect()
+
+	ids := []int64{}
+
+	var bogs1 []Bog
+	var done1 bool
+	go func() {
+		log.Println("In {Go1}")
+		Contactizer.GET(&bogs1, ids)
+		log.Printf("Go1: There were %d bogs", len(bogs1))
+		done1 = true
+	}()
+
+	var bogs2 []Bog
+	var done2 bool
+	go func() {
+		log.Println("In {Go2}")
+		Contactizer.GET(&bogs2, ids)
+		log.Printf("Go2: There were %d bogs", len(bogs2))
+		done2 = true
+	}()
+
+	for !(done1 || done2) {
+		fmt.Println("waiting...")
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	// var input string
+	// fmt.Scanln(&input)
+	// fmt.Println("done")
+
+}
