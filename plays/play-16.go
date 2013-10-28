b5e581eece400d6b541b210a0e3a0c3f67c8b177
Ali Anwar <allochi@gmail.com>
2013-10-28 12:32:19 +0100

    Requests, Entries and Back from 17

--- plays/play-16.go
+++ plays/play-16.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	// "strings"
 )
 
@@ -11,35 +12,69 @@ type Entry struct {
 	values   interface{}
 }
 
+func (e *Entry) Bake() string {
+	field := e.field
+	switch field {
+	case "Id":
+		fallthrough
+	case "CreatedAt":
+		fallthrough
+	case "UpdatedAt":
+		field = fmt.Sprintf("'%s'", field)
+	default:
+		field = fmt.Sprintf("data->>'%s'", field)
+	}
+	return "..."
+}
+
 type Requester interface {
 	Where(Entry) *Requester
-	// And(Entry) *Requester
+	And(Entry) *Requester
 	// Or(Entry) *Requester
 	Bake() string
 }
 
-type RequestChain []Entry
+type RequestChain struct {
+	entries    []Entry
+	operations []string
+}
 
 func (rc *RequestChain) Where(e Entry) *RequestChain {
-	// rc = append(rc, e)
+	rc.entries = append(rc.entries, e)
+	rc.operations = append(rc.operations, "WHERE")
+	return rc
+}
+
+func (rc *RequestChain) And(e Entry) *RequestChain {
+	rc.entries = append(rc.entries, e)
+	rc.operations = append(rc.operations, "AND")
+	return rc
+}
+
+func (rc *RequestChain) Or(e Entry) *RequestChain {
+	rc.entries = append(rc.entries, e)
+	rc.operations = append(rc.operations, "Or")
 	return rc
 }
 
 func (rc *RequestChain) Bake() string {
-	// return strings.Join(rc[0], ",")
-	return ""
+	conditions := ""
+	for idx, op := range rc.operations {
+		conditions += " " + op + " " + rc.entries[idx].Bake()
+	}
+	return conditions
 }
 
 func main() {
 
-	// e1 := Entry{"country_id", Operator.IN, []int64{1, 2, 3, 4, 5}}
-	// e2 := Entry{"title", "Like", "Cheif %"}
+	e1 := Entry{"country_id", "IN", []int64{1, 2, 3, 4, 5}}
+	e2 := Entry{"title", "LIKE", "Cheif %"}
 
-	// var rc Requester = make(RequestChain)
-	var rc Requester = make(RequestChain)
-	// rc.Where(e2)
+	rc := &RequestChain{}
+	rc.Where(e1).And(e2)
 
 	// fmt.Println(rc.Bake())
-	fmt.Println(rc)
+	fmt.Printf("%s\n", rc.Bake())
+	spew.Dump(rc)
 
 }
